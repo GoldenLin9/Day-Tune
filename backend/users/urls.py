be0150b57235e5ad160104d3421fe3520d09e2cf
GoldenLin9 <goldenlin0909@gmail.com>
2024-08-02 19:44:19 -0400

    partial Google OAuth setup

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -9,4 +9,8 @@ from . import views
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    path('logout', views.logout, name='logout'),
+    path('auth_receiver', views.auth_receiver, name='auth_receiver'),
+    path('login', views.login, name='login'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+import os
+from django.http import HttpResponse
+from google.auth.transport import requests
+from google.oauth2 import id_token
+import jwt
+
 
 # Create your views here.
 
 # def test(req):
 #     return HttpResponse("Hello, world. You're at the goals index.")
+
+def login(req):
+
+    return HttpResponse("Hello, world. You're at the login index.")
+
+def auth_receiver(req):
+
+    token = req.POST['credential']
+
+    try:
+        user_data = id_token.verify_oauth2_token(token, req.Request(), os.getenv('GOOGLE_CLIENT_ID'))
+    except ValueError:
+        return HttpResponse("Invalid token")
+    
+    req.session['user'] = user_data
+    return HttpResponse("Logged in")
+
+def logout(req):
+    del req.session['user']
+    return HttpResponse("Logged out")
